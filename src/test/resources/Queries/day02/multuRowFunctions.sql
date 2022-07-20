79219099da764630e666b51c8b3571b4dfe43aa9
zivadinjo <zivadinjo1985@gmail.com>
2022-07-19 20:35:59 -0500

    day02

--- src/test/resources/Queries/day02/multuRowFunctions.sql
+++ src/test/resources/Queries/day02/multuRowFunctions.sql
@@ -11,6 +11,8 @@ MULTI-ROW / AGGREGATE / GROUPING FUNC
 
  it will take multiple row and gives single output
 
+All aggregate functions does not count null values
+
  */
 --Gives you all different departments in this case departments --> count() returns number of given table,row,name,etc..
  select count(*) from DEPARTMENTS;
@@ -23,3 +25,28 @@ select count(distinct FIRST_NAME) from EMPLOYEES;
 select count(*) from EMPLOYEES
 where JOB_ID IN ('IT_PROG','SA_REP');
 --If you want to count your result,use count(*) to get correct answer
+
+--How many employees we have ase department_id is null
+select count(*) from EMPLOYEES
+where DEPARTMENT_ID is null ;
+
+--MAX
+--Get me max salary from employees
+select max(SALARY) from EMPLOYEES;
+
+--MIN
+--Get me min salary from employees
+select min(SALARY) from EMPLOYEES;
+
+--SUM
+--Get me total salary of employees
+select sum(SALARY) from EMPLOYEES;
+
+--AVG
+--Get me average salary from employees
+select avg(SALARY) from EMPLOYEES;
+
+--ROUND --> It is not aggregate function.It is numeric function
+select round(avg(SALARY)) from EMPLOYEES;
+
+select round(avg(SALARY),1) from EMPLOYEES; -- One decimal is added with number 1,two with number 2, etc
