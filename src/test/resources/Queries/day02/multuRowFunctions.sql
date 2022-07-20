54224d7b6269f04875a29f0405ef51f4f1c2cb9b
zivadinjo <zivadinjo1985@gmail.com>
2022-07-19 20:01:07 -0500

    day02

--- src/test/resources/Queries/day02/multuRowFunctions.sql
+++ src/test/resources/Queries/day02/multuRowFunctions.sql
@@ -12,8 +12,14 @@ MULTI-ROW / AGGREGATE / GROUPING FUNC
  it will take multiple row and gives single output
 
  */
-
+--Gives you all different departments in this case departments --> count() returns number of given table,row,name,etc..
  select count(*) from DEPARTMENTS;
 
 --How many different first names we have?
 select distinct FIRST_NAME from EMPLOYEES;
+select count(distinct FIRST_NAME) from EMPLOYEES;
+
+--How many employees we have work as 'IT_PROG' or 'SA_REP'
+select count(*) from EMPLOYEES
+where JOB_ID IN ('IT_PROG','SA_REP');
+--If you want to count your result,use count(*) to get correct answer
