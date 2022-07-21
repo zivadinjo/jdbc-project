8f26f4e482e3d6a663130f1e65b0aee5ab338d7d
zivadinjo <zivadinjo1985@gmail.com>
2022-07-20 19:14:50 -0500

    day02

--- src/test/resources/Queries/day02/gropuBy.sql
+++ src/test/resources/Queries/day02/gropuBy.sql
@@ -28,3 +28,23 @@ select DEPARTMENT_ID ,round(avg(SALARY)),count(*) from EMPLOYEES
 where DEPARTMENT_ID is not null
 group by DEPARTMENT_ID
 order by DEPARTMENT_ID;
+
+-- get me how many departments we have in each location
+select LOCATION_ID,count(*) from DEPARTMENTS
+group by LOCATION_ID;
+
+-- get me how many locations we have in each country
+select COUNTRY_ID,count(*) from LOCATIONS
+group by COUNTRY_ID
+order by count(*) asc ; -- sort them based on number of locations
+
+-- get me how many countries we have in each region
+select REGION_ID,count(*) from COUNTRIES
+group by REGION_ID
+order by REGION_ID; -- order them by region id
+
+-- get me how many employees working for each manager
+select MANAGER_ID,count(*) from EMPLOYEES
+where MANAGER_ID is not null -- ignore null values
+group by MANAGER_ID
+having count(*)>5; -- giving result where count number is bigger than 5,manager having more than 5 employees
