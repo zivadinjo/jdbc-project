051ac2d0ea9f56b1f7b6482501086c6ae6431516
zivadinjo <zivadinjo1985@gmail.com>
2022-07-23 15:34:45 -0500

    day04

--- src/test/resources/Queries/day04/joins.sql
+++ src/test/resources/Queries/day04/joins.sql
@@ -24,9 +24,40 @@ on C.ADDRESS_ID = A.ADDRESS_ID;
  LEFT OUTER JOIN
 
  Matching part and also unique parts for left table
+ from CUSTOMER C right outer join ADDRESS A
+ there is not difference between left outer join and left join
 
  */
 
 select FIRST_NAME,LAST_NAME,A.ADDRESS_ID,C.ADDRESS_ID,ADDRESS,PHONE
 from CUSTOMER C left outer join ADDRESS A
                            on C.ADDRESS_ID = A.ADDRESS_ID;
+
+/*
+
+ RIGHT OUTER JOIN
+ Matching part and also unique part for right table
+ from CUSTOMER C(left table) right outer join ADDRESS A(right table)
+ and also we can switch places for customer A and address A,after from and also
+ switch on part C address_id and A address_id
+ there is not any difference between right outer join and right join
+
+ */
+
+select FIRST_NAME,LAST_NAME,A.ADDRESS_ID,C.ADDRESS_ID,ADDRESS,PHONE
+from CUSTOMER C right outer join ADDRESS A
+                                on C.ADDRESS_ID = A.ADDRESS_ID;
+
+select FIRST_NAME,LAST_NAME,A.ADDRESS_ID,C.ADDRESS_ID,ADDRESS,PHONE
+from ADDRESS A LEFT JOIN CUSTOMER C
+                         ON A.ADDRESS_ID=C.ADDRESS_ID;
+
+/*
+
+ FULL OUTER JOIN
+
+ */
+
+select FIRST_NAME,LAST_NAME,A.ADDRESS_ID,C.ADDRESS_ID,ADDRESS,PHONE
+from ADDRESS A FULL JOIN CUSTOMER C
+                         ON A.ADDRESS_ID=C.ADDRESS_ID;
